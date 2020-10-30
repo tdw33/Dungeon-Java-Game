32f5bc39334abc90786e340a0edb0f7b70fafd87
lw955 <lw955@bath.ac.uk>
2020-10-30 13:55:28 +0000

    added dungeoncrafter code with basic movement of character block

---
+++ DungeonCrafter/core/src/Screens/MainGameScreen.java
@@ -0,0 +1,156 @@
+package Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import dev.desktop.dungeoncrafter.DungeonCrafter;
+
+public class MainGameScreen implements Screen, InputProcessor {
+
+    TextureAtlas atlas;
+    SpriteBatch batch;
+    boolean movingRight = false;
+    boolean movingLeft = false;
+    boolean movingUp = false;
+    boolean movingDown = false;
+    Sprite sprite;
+
+
+    DungeonCrafter game;
+
+    public MainGameScreen(DungeonCrafter game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+        atlas = new TextureAtlas("textures.atlas");
+
+        sprite = new Sprite(atlas.findRegion("tile/wall"));
+        sprite.setPosition(0,
+                0);
+
+        Gdx.input.setInputProcessor(this);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        if(movingRight) {
+            sprite.translateX(3f);
+        }
+
+        if(movingLeft) {
+            sprite.translateX(-3f);
+        }
+        if(movingUp) {
+            sprite.translateY(3f);
+        }
+        if(movingDown) {
+            sprite.translateY(-3f);
+        }
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        game.batch.draw(sprite, sprite.getX(), sprite.getY(), 100,100); // this will be diffrent when you have nummbers at end eg player_1, player_2
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        atlas.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.LEFT) {
+            movingLeft = true;
+        }
+        if(keycode == Input.Keys.RIGHT) {
+            movingRight = true;
+        }
+        if(keycode == Input.Keys.UP) {
+            movingUp = true;
+        }
+        if(keycode == Input.Keys.DOWN) {
+            movingDown = true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if(keycode == Input.Keys.RIGHT)
+            movingRight = false;
+
+        if(keycode == Input.Keys.LEFT)
+            movingLeft = false;
+
+        if(keycode == Input.Keys.UP)
+            movingUp = false;
+
+        if(keycode == Input.Keys.DOWN)
+            movingDown = false;
+
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
---
+++ DungeonCrafter/desktop/src/dev/desktop/dungeoncrafter/desktop/DesktopLauncher.java
@@ -0,0 +1,37 @@
+package dev.desktop.dungeoncrafter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import dev.desktop.dungeoncrafter.DungeonCrafter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+
+		config.title = "Working?";
+		config.width = DungeonCrafter.WIDTH;
+		config.height = DungeonCrafter.HEIGHT;
+		config.resizable = true;
+
+		config.foregroundFPS = 60; //fps for when game is being played
+
+
+		//Pack all textures into texture atlas
+		//TODO: remove code before release and just release game with pre packed atlas
+
+		TexturePacker.Settings sets = new TexturePacker.Settings();
+		sets.pot =true; // sets size of images
+		sets.fast = true; // good for dev make texture packer run faster but can be changed to false before release
+		sets.combineSubdirectories = true;
+		sets.paddingX = 1; //space out images in atlas to help with texture bleeding
+		sets.paddingY = 1;
+		sets.edgePadding = true;
+		TexturePacker.process(sets, "raw_textures", "./", "textures"); //pack the textures
+
+
+
+		new LwjglApplication(new DungeonCrafter(), config);
+	}
+}
