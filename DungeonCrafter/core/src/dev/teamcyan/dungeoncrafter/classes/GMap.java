42578e7b098ab994a071000b74454440a975b664
vpl22 <vpl22@bath.ac.uk>
2020-11-02 12:28:20 +0000

    Created GMap GameElement and Pos classes

---
+++ DungeonCrafter/core/src/dev/teamcyan/dungeoncrafter/classes/GMap.java
@@ -0,0 +1,17 @@
+package dev.teamcyan.dungeoncrafter.classes;
+import java.util.*;
+import dev.teamcyan.dungeoncrafter.classes.GameElement;
+
+public class GMap{
+
+  private int sizeHorizontal;
+  private int sizeVertical;
+  private String seed;
+  private gMap Vector<Vector<GameElement>>;
+
+  public GameElement getGEAtPos(Pos position){
+    //returns 
+    return this.gMap.at
+
+
+}
---
+++ DungeonCrafter/core/src/dev/teamcyan/dungeoncrafter/classes/GameElement.java
@@ -0,0 +1,7 @@
+package dev.teamcyan.dungeoncrafter.classes;
+import java.util.*;
+import dev.teamcyan.dungeoncrafter.classes.GameElement;
+
+public class GameElement {
+  
+}
