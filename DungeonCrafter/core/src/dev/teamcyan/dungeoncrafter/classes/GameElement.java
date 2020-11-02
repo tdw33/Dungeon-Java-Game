2f13621d2477d6d58dffc45b12b877bad2030c65
vpl22 <vpl22@bath.ac.uk>
2020-11-02 12:37:20 +0000

    update: sketched out the definition of GameElement

--- DungeonCrafter/core/src/dev/teamcyan/dungeoncrafter/classes/GameElement.java
+++ DungeonCrafter/core/src/dev/teamcyan/dungeoncrafter/classes/GameElement.java
@@ -1,7 +1,42 @@
-package dev.teamcyan.dungeoncrafter.classes;
+/package dev.teamcyan.dungeoncrafter.classes;
 import java.util.*;
-import dev.teamcyan.dungeoncrafter.classes.GameElement;
+import dev.teamcyan.dungeoncrafter.classes.*;
 
 public class GameElement {
-  
+  private String uid;
+  private Pos position;
+  private bool visible;
+  private integer health;
+  private Sprite sprite;
+  private bool collision;
+
+  public void render(){
+    // method for rendering
+  }
+
+  public void render()
+  {
+    // method for rendering
+  }
+
+  public void create()
+  {
+    // method for creating the GE
+  }
+
+  public void destroy()
+  {
+  }
+
+  public void interactA()
+  {
+  }
+
+  public void interactB()
+  {
+  }
+
+  public void die()
+  {
+  }
 }
